62a03ee5cba59f3b0d42b832968c63199a385975
LexYu <84486286+TLexYuW@users.noreply.github.com>
2024-11-13 21:51:38 +0800

    update

---
+++ go-sqlc/go.mod
@@ -0,0 +1,5 @@
+module github.com/lexyu-golang-project-collection/go-frameworks-collection/go-sqlc
+
+go 1.23.1
+
+require github.com/mattn/go-sqlite3 v1.14.24 // indirect
---
+++ go-sqlc/query.sql
@@ -0,0 +1,26 @@
+-- name: GetAuthor :one
+SELECT *
+FROM authors
+WHERE id = ?
+LIMIT 1;
+
+-- name: ListAuthors :many
+SELECT *
+FROM authors
+ORDER BY name;
+
+-- name: CreateAuthor :one
+INSERT INTO authors (name, bio)
+VALUES (?, ?)
+RETURNING *;
+
+-- name: UpdateAuthor :one
+UPDATE authors
+set name = ?,
+bio = ?
+WHERE id = ?
+RETURNING *;
+
+-- name: DeleteAuthor :exec
+DELETE FROM authors
+WHERE id = ?;
